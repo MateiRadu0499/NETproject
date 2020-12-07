a7d5e06d5370cc90d90094c4b644fa99944df804
MateiRadu0499 <43829145+MateiRadu0499@users.noreply.github.com>
2020-12-08 01:46:54 +0200

    Added skeleton and basic user(later for login/register) API calls

---
+++ BDefenderApp/BDefenderApp/Model/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BDefenderApp.Model
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ BDefenderApp/BDefenderApp/Model/User.cs
@@ -0,0 +1,19 @@
+﻿namespace BDefenderApp.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public string Affiliation { get; set; } = "None";
+        public bool IsDeleted { get; set; }
+
+        public void MarkAsDeleted()
+        {
+            IsDeleted = true;
+        }
+    }
+}
